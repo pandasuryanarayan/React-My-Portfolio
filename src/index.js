854f09f0093ad868f23b35955f71fd0695eb6b15
Suryanarayan Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2024-07-02 20:04:22 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@
  * Entry point of application, where App is rendered within the div with the id of "app" 
  */
 
-import React from "react";
-import { render } from "react-dom";
+// import React from "react";
+// import { render } from "react-dom";
 
+// import App from "./App";
+
+// render(<App></App>, document.getElementById("app"));
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-render(<App></App>, document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
